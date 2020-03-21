2b04a98ab1abc108f7fa9e382f22bf6022ee490a
Stefan Hoferer <s.hoferer@hse24.de>
2020-03-21 23:50:16 +0100

    fix ktlint

--- src/main/kotlin/app/colivery/api/config/FirestoreConfig.kt
+++ src/main/kotlin/app/colivery/api/config/FirestoreConfig.kt
@@ -5,12 +5,12 @@ import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
+import java.io.FileInputStream
+import javax.validation.constraints.NotBlank
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import java.io.FileInputStream
-import javax.validation.constraints.NotBlank
 
 const val USER_COLLECTION_NAME = "user"
 const val ORDER_ITEM_COLLECTION_NAME = "order_item"
