4155db811892889abe9574bbf1d190f23428233d
Stefan Hoferer <s.hoferer@hse24.de>
2020-03-22 17:25:05 +0100

    allow only localhost:4444

--- src/main/kotlin/app/colivery/api/api/UserResources.kt
+++ src/main/kotlin/app/colivery/api/api/UserResources.kt
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -36,7 +35,9 @@ class UserResources(
     }
 
     @GetMapping(produces = [APPLICATION_JSON_VALUE])
-    fun getUser(@RequestParam(name = "user_id") userId: String): FirestoreUser {
+    fun getUser(): FirestoreUser {
+        val (userId) = securityUtils.principal
+            ?: throw UnauthorizedException()
         return userService.findUser(userId = userId)
     }
 }
